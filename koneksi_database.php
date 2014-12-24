c4eeffc5b7ba81995a3101acc9689e5c9d80da0b
ayub06 <ayubalandroid@gmail.com>
2014-12-24 20:55:26 +0700

    MYSQL

--- koneksi_database.php
+++ koneksi_database.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "5halahudin";
-$database = "pendaftaran-acfc";
+$host = "mysql17.000webhost.com";
+$username = "a9251276_ayub";
+$password = "5h4lahudin";
+$database = "a9251276_ayub";
 $koneksi = mysql_connect($host, $username, $password);
 
 $pilihdatabase = mysql_select_db($database, $koneksi);
